c758dcaf1eb3cc0101cea3d5225d6552a6f1a7c9
Corey Crosby <ccc_13@live.ca>
2019-08-15 16:13:49 -0400

    Updates

---
+++ precache-manifest.4edf01034138f65ac111633c52f6f192.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1115004bfba807051a81885beef888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a74ffe6c335a5954d71",
+    "url": "/static/css/main.0fb2ec3f.chunk.css"
+  },
+  {
+    "revision": "14c0e8d19b5afcfd0568",
+    "url": "/static/js/2.95a825d8.chunk.js"
+  },
+  {
+    "revision": "4a74ffe6c335a5954d71",
+    "url": "/static/js/main.24ae683d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f3ad8688f0e12f95d8a297c6f1ed9de",
+    "url": "/static/media/fridgenerate.3f3ad868.jpg"
+  },
+  {
+    "revision": "2bf59744ec3abde25845762157cf9cd2",
+    "url": "/static/media/headshot.2bf59744.jpg"
+  },
+  {
+    "revision": "e6874b014955f71b84c5c442d643e2e5",
+    "url": "/static/media/punchstarter.e6874b01.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal_website/precache-manifest.cf7e7e99d48368574cbb96ef8f5512cd.js"
+  "/precache-manifest.4edf01034138f65ac111633c52f6f192.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal_website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
